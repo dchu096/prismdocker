b6054910fb63978e25c4fd62333aa443c0ab0ecc
Daniel Chu <danielchu096@gmail.com>
2024-05-19 22:27:19 +1200

    Update entrypoint.sh

--- java/entrypoint.sh
+++ java/entrypoint.sh
@@ -14,22 +14,21 @@ export INTERNAL_IP
 
 # Replace Startup Variables
 # shellcheck disable=SC2086
-MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+MODIFIED_STARTUP=$(echo -e "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo -e "STARTUP /home/container: ${MODIFIED_STARTUP}"
 
 # Run the Server
 # shellcheck disable=SC2086
-echo "     __  __           __  _             ____                           __    "
-echo "    / / / /___  _____/ /_(_)___  ____ _/ __ \___  ____ ___  ____ _____/ /__  "
-echo "   / /_/ / __ \/ ___/ __/ / __ \/ __ `/ /_/ / _ \/ __ `__ \/ __ `/ __  / _ \ "
-echo "  / __  / /_/ (__  ) /_/ / / / / /_/ / _, _/  __/ / / / / / /_/ / /_/ /  __/ "
-echo " /_/ /_/\____/____/\__/_/_/ /_/\__, /_/ |_|\___/_/ /_/ /_/\__,_/\__,_/\___/  "
-echo "                              /____/                                         "
+cat << 'EOF'
+    __  __           __  _             ____                           __   
+   / / / /___  _____/ /_(_)___  ____ _/ __ \___  ____ ___  ____ _____/ /__ 
+  / /_/ / __ \/ ___/ __/ / __ \/ __ `/ /_/ / _ \/ __ `__ \/ __ `/ __  / _ \
+ / __  / /_/ (__  ) /_/ / / / / /_/ / _, _/  __/ / / / / / /_/ / /_/ /  __/
+/_/ /_/\____/____/\__/_/_/ /_/\__, /_/ |_|\___/_/ /_/ /_/\__,_/\__,_/\___/ 
+                             /____/                                         
+EOF
+
 echo -e "========================================================================================================"
-echo -e "${CYAN}Welcome to Gravel Host! If you have any issues don't hesitate to contact us on discord at discord.hostingremade.com ${RESET_COLOR}"
+echo -e "${CYAN}Welcome to HR! If you have any issues don't hesitate to contact us on discord at discord.hostingremade.com ${RESET_COLOR}"
 echo -e "========================================================================================================"
-eval ${MODIFIED_STARTUP}
-
-
-
-
+eval "${MODIFIED_STARTUP}"
